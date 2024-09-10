1caa42969674dc32b09d6d63fcdbb1f6dbdd77a0
tassun <32536248+tassun@users.noreply.github.com>
2024-09-10 09:26:02 +0700

    Update bc.js

--- public/js/bc.js
+++ public/js/bc.js
@@ -8,7 +8,13 @@ function startReceiveBroadcast() {
             let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
             link.click(function() { div.remove(); });
             let span = $("<span></span>").html(msg.message);
-            div.append(link).append(span).appendTo($("body"));
+            let body = $("body");
+            let isz = body.find("div.bc-layer").length;
+            if(isz > 0 && isz < 10) {
+                let bottom = isz * 50;
+                div.css({bottom: bottom});
+            }
+            div.append(link).append(span).appendTo(body);
         });
     }
 }
