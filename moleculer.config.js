e02125fe852fdc7aa2a45394095e6965b6e1aa22
tassun <32536248+tassun@users.noreply.github.com>
2024-12-08 11:14:44 +0700

    Update moleculer.config.js

--- moleculer.config.js
+++ moleculer.config.js
@@ -10,7 +10,8 @@ module.exports = {
                 color: true,
                 formatter: "full",
             } 
-        },                   
+        },
+		/*		
         {
             type: "File",
             options: {
@@ -21,7 +22,7 @@ module.exports = {
                 eol: "\n",
                 interval: 1000,
             }
-        }
+        }*/
     ],
     registry: {
         strategy: "RoundRobin",
